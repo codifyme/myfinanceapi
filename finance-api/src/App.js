18edba0255a9ef35d43c1118bcc7c8a9432cecca
Angel Cochachez <acochachez@gmail.com>
2020-11-12 16:20:06 -0800

    Added components folder, creating the button components and Nav Menu

---
+++ finance-api/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/pages/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+     <Navbar />
+     <Switch>
+       <Route path='/' />
+     </Switch>
+    </Router>
+  );
+}
+
+export default App;
