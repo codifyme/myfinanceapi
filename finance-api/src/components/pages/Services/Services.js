0e84028041711f892041aa86711faa8eb5d21a19
Angel Cochachez <acochachez@gmail.com>
2020-11-14 02:20:37 -0800

    fixed the bugs and adding an external link in footer

---
+++ finance-api/src/components/pages/Services/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import { homeObjOne} from './Data';
+
+
+function Services() {
+  return (
+    <>
+      
+      <HeroSection {...homeObjOne} />
+    </>
+  );
+}
+
+export default Services;
