a109b4eb9e2ccab228d11d42f68d0751b3512d3b
Angel Cochachez <acochachez@gmail.com>
2020-11-16 13:31:45 -0800

    first heroku deploy

---
+++ back-end/server.js
@@ -0,0 +1,16 @@
+// const app=express();
+// const PORT=process.env.PORT ||8080;
+
+// const routes=require('./routes/api');
+
+// mongoose.connection.on(process.env.MONGODB_URI|| mongodb://localhost/myfinance',{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// });
+
+// mongoose.connection.on('connected', ()=>{
+//     console.log('Mongoose is conneceted!!!');
+// });
+
+// //Data parsin
+// app.use(express.json());
